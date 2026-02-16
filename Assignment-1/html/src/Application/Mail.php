af8c92f910a6e67bbbade47f20db3c00dfdc4623
Sonu <sonukumari.mahatopanjiyar@dcmail.ca>
2026-02-15 20:39:27 -0500

    Fetch, prepare query and delete
    Fetchinf all records through id, preparing query, returning null if record doesnot exists and executing delete query

--- Assignment-1/html/src/Application/Mail.php
+++ Assignment-1/html/src/Application/Mail.php
@@ -17,4 +17,42 @@ class Mail {
 
         return $stmt->fetchColumn();
     }
+    public function getAll(): array
+    {
+        // Fetch records by id
+        $stmt = $this->pdo->query(
+            "SELECT id, subject, body FROM mail ORDER BY id ASC"
+        );
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getById(int $id): ?array
+    {
+        // query with id
+        $stmt = $this->pdo->prepare(
+            "SELECT id, subject, body FROM mail WHERE id = :id"
+        );
+
+        $stmt->execute([':id' => $id]);
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // Return null if record not exists
+        return $row ? $row : null;
+    }
+
+    public function deleteById(int $id): bool
+    {
+        // execute delete query
+        $stmt = $this->pdo->prepare(
+            "DELETE FROM mail WHERE id = :id"
+        );
+
+        $stmt->execute([':id' => $id]);
+
+        // Checking if one row is delete or not
+        return $stmt->rowCount() === 1;
+    }
+}
 }
